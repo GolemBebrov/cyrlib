5338011cd98a825852fdb7fd47bc201d17a749be
GolemBebrov <bebrovgolem@gmail.com>
2026-09-20 18:34:24 +0300

    Added:  texture_pro, text, text_ex

--- src/cyrlib/draw/draw.pyx
+++ src/cyrlib/draw/draw.pyx
@@ -1,13 +1,15 @@
 from cyrlib.raylib.raylib cimport (
     DrawFPS, ClearBackground, BeginDrawing, EndDrawing, 
-    DrawTexture, DrawTextureV, DrawTextureEx, DrawTextureRec
+    DrawTexture, DrawTextureV, DrawTextureEx, DrawTextureRec, 
+    DrawTexturePro, DrawTextEx, DrawText
 )
 from cyrlib.texture.texture cimport Texture
 from cyrlib.color.color cimport Color, RlColor, color_new
 from cyrlib.vector2.vector2 cimport vector2_t, vector2_new, Vector2
 from cyrlib.rect.rect cimport Rectangle, Rect, rectangle_new
+from cyrlib.font.font cimport Font
 cdef inline vector2_t __parse_dest_like(object dest) except *:
-    if isinstance(dest, Vector2):
+    if type(dest) is Vector2:
         return (<Vector2>dest).data
     elif isinstance(dest, list | tuple):
         if len(dest) != 2:
@@ -17,7 +19,7 @@ cdef inline vector2_t __parse_dest_like(object dest) except *:
         raise TypeError(f"dest_like argument must be Vector2, tuple, or list, got {type(dest).__name__}")
 
 cdef inline RlColor __parse_color_like(object color) except *:
-    if isinstance(color, Color):
+    if type(color) is Color:
         return (<Color>color)._raw
     elif isinstance(color, list | tuple):
         if not (2 < len(color) < 5):
@@ -27,7 +29,7 @@ cdef inline RlColor __parse_color_like(object color) except *:
         raise TypeError(f"color_like argument must be Color, tuple, or list, got {type(color).__name__}")
 
 cdef inline Rectangle __parse_rect_like(object rect) except *:
-    if isinstance(rect, Rect):
+    if type(rect) is Rect:
         return (<Rect>rect)._raw
     elif isinstance(rect, list | tuple):
         if len(rect) != 4:
@@ -87,3 +89,22 @@ cpdef void texture_rec(Texture tex, object rect, object pos, object tint):
     cdef Rectangle rectangle = __parse_rect_like(rect)
     cdef vector2_t vec_pos = __parse_dest_like(pos)
     DrawTextureRec(tex._raw, rectangle, vec_pos, color_tint)
+
+cpdef void texture_pro(Texture tex, object source_rect, object dest_rect, object origin, float rotation, object tint):
+    cdef RlColor color_tint = __parse_color_like(tint)
+    cdef Rectangle c_source_rect, c_dest_rect
+    c_source_rect = __parse_rect_like(source_rect)
+    c_dest_rect = __parse_rect_like(dest_rect)
+    cdef vector2_t vec_origin = __parse_dest_like(origin)
+    DrawTexturePro(tex._raw, c_source_rect, c_dest_rect, vec_origin, rotation, color_tint)
+
+cpdef void text(str txt, int pos_x, int pos_y, int font_size, object color):
+    cdef bytes b_text = txt.encode("utf-8")
+    cdef RlColor color_tint = __parse_color_like(color)
+    DrawText(<const char *>b_text, pos_x, pos_y, font_size, color_tint)
+
+cpdef void text_ex(Font font, str text, object pos, float font_size, float spacing, object tint):
+    cdef vector2_t vec_pos = __parse_dest_like(pos)
+    cdef RlColor col = __parse_color_like(tint)
+    cdef bytes b_text = text.encode("utf-8")
+    DrawTextEx(font._raw, b_text, vec_pos, font_size, spacing, col)
